870ab42456f663287d7a945fdfdd48e6a75b223a
eliaslevander <144018111+eliaslevander@users.noreply.github.com>
2024-04-25 12:07:13 +0200

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,7 @@
     <h1>About brandz</h1>
     <p>
       <strong>brandz</strong> is a mock-up e-commerce platform that I created
-      for a school assignment. It is made built using Vue 3 and TypeScript, with Vuetify
+      for a school assignment. It is built using Vue 3 and TypeScript, with Vuetify
       serving as its UI-framework.
     </p>
   </div>
